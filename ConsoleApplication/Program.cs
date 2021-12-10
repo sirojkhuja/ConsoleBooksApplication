b9469c644ef145d0a2ff29f719acb67dc7abd895
Sirojkhuja Muminkhodjaev <sirojxuja@gmail.com>
2021-12-11 00:37:24 +0500

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    internal class Tool
+    {
+
+    }
+}
